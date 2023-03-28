0065781657c5b4ea0cc33332fb4084cdc6843979
glutenfreedata <luteriouniversidad@gmail.com>
2023-03-28 12:36:45 +0200

    primer commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const JsonFileAdapter = require('@bot-whatsapp/database/json')
+
+const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+
+const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+    [
+        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
+        'https://bot-whatsapp.netlify.app/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+    [
+        '🙌 Aquí encontras un ejemplo rapido',
+        'https://bot-whatsapp.netlify.app/docs/example/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
+    [
+        '🚀 Puedes aportar tu granito de arena a este proyecto',
+        '[*opencollective*] https://opencollective.com/bot-whatsapp',
+        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+        '[*patreon*] https://www.patreon.com/leifermendez',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo','buenas','buenos dias', 'buenos días','buenas tardes', 'buenas noches'])
+    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+    .addAnswer(
+        [
+            'soy Eleuterio Hernández y he creado una encuesta',
+            'para superar el ejercicio de esta semana del Lab 01 ',
+            'de Investigación de Mercados con la suposición de que Netflix necesita información sobre sus usuarios',
+            '👉 Te agradezco muchísimo tu participación, tu tiempo es oro',
+        ],
+        null,
+        null
+    )
+    .addAnswer('¿Cual es tu nombre?', {capture:true}, (ctx) => {
+        console.log('Info usuario: ', ctx.body)
+    })
+
+const main = async () => {
+    const adapterDB = new JsonFileAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
